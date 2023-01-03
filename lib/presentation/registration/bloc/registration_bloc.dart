e919965a8463b8f1e43cfcf85e656075a153619b
RNOVOSELOV <novoselov_roman@mail.ru>
2023-01-03 11:51:46 +0300

    SVG  registration avatar builder

--- lib/presentation/registration/bloc/registration_bloc.dart
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -10,9 +11,24 @@ part 'registration_event.dart';
 part 'registration_state.dart';
 
 class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
-  RegistrationBloc() : super(RegistrationFieldsInfo(avatarLink: '')) {
-    on<RegistrationEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  static const _defaultAvatarKey = 'test';
+
+  static String _avatarBuilder(String key) =>
+      'https://avatars.dicebear.com/api/croodles/$key.svg';
+
+  String _avatarKey = _defaultAvatarKey;
+
+  RegistrationBloc()
+      : super(RegistrationFieldsInfo(
+            avatarLink: _avatarBuilder(_defaultAvatarKey))) {
+    on<RegistrationChangeAvatar>(_onChangeAvatar);
+  }
+
+  FutureOr<void> _onChangeAvatar(
+    final RegistrationChangeAvatar event,
+    final Emitter<RegistrationState> emit,
+  ) {
+    _avatarKey = Random().nextInt(1000000).toString();
+    emit(RegistrationFieldsInfo(avatarLink: _avatarBuilder(_avatarKey)));
   }
 }
--- lib/presentation/registration/view/registration_page.dart
+++ lib/presentation/registration/view/registration_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gift_manager/extentions/build_context.dart';
 import 'package:gift_manager/extentions/theme_extensions.dart';
 import 'package:gift_manager/presentation/registration/bloc/registration_bloc.dart';
@@ -53,42 +54,59 @@ class _RegistrationPageWidgetState extends State<_RegistrationPageWidget> {
       appBar: AppBar(),
       body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 16),
-            padding:
-                const EdgeInsets.only(left: 8, top: 6, bottom: 6, right: 4),
-            decoration: BoxDecoration(
-              color: context.dynamicPlaneColor(
-                lightThemeColor: AppColors.lightLightBlue100,
-                darkThemeColor: AppColors.darkWhite20,
-              ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  height: 48,
-                  width: 48,
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  'Ваш аватар',
-                  style: context.theme.h3,
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                const Spacer(),
-                TextButton(
-                  onPressed: () => context
-                      .read<RegistrationBloc>()
-                      .add(const RegistrationChangeAvatar()),
-                  child: const Text('Изменить'),
-                ),
-              ],
-            ),
+          const _AvatarWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.only(left: 8, top: 6, bottom: 6, right: 4),
+      decoration: BoxDecoration(
+        color: context.dynamicPlaneColor(
+          lightThemeColor: AppColors.lightLightBlue100,
+          darkThemeColor: AppColors.darkWhite20,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          BlocBuilder<RegistrationBloc, RegistrationState>(
+            buildWhen: (_, current) => current is RegistrationFieldsInfo,
+            builder: (context, state) {
+              final fieldsInfo = state as RegistrationFieldsInfo;
+              return SvgPicture.network(
+                fieldsInfo.avatarLink,
+                height: 48,
+                width: 48,
+              );
+            },
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            'Ваш аватар',
+            style: context.theme.h3,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          const Spacer(),
+          TextButton(
+            onPressed: () => context
+                .read<RegistrationBloc>()
+                .add(const RegistrationChangeAvatar()),
+            child: const Text('Изменить'),
           ),
         ],
       ),
